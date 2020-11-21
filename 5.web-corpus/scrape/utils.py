b001db5a09288c05b715951eff7ab95bdf5abe6e
Alexandre Roulois <alexandre.roulois@u-paris.fr>
2020-11-21 19:30:38 +0100

    Create a corpus of movie reviews

--- 5.web-corpus/scrape/utils.py
+++ 5.web-corpus/scrape/utils.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-#
-#   Modules to import
-#
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -18,4 +15,17 @@ def get_html_from_url(url, charset='utf-8'):
     request = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(request) as f:
         html = f.read().decode(charset)
+
     return html
+
+def parse_html_by_class(html, selector):
+    """Extracts tags from HTML whith CSS selector.
+    
+    Keyword arguments:
+    html -- the html page
+    selector -- the CSS selector
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup.select(selector)
+
+    return tags
